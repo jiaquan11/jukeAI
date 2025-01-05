4f3356c9e2df831afc38f9c0da9c81905eb543cb
jiaquan11 <913882743@qq.com>
2025-01-05 23:23:24 +0800

    huggingface-learn:在线模型和本地模型的加载和使用

---
+++ huggingface-learn/API_test/api_test02.py
@@ -0,0 +1,10 @@
+import requests
+
+#这是一个续写生成模型，不是问答模型
+API_URL = "https://api-inference.huggingface.co/models/uer/gpt2-chinese-cluecorpussmall"
+API_TOKEN = "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"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+#使用Token进行访问在线模型
+response = requests.post(API_URL, headers=headers, json={'inputs': '您好，Hugging face'})
+print(response.json())
